ed904a14839c61670136540a0c9ba8c509250a6e
DavidBlomeyer <89857190+DavidBlomeyer@users.noreply.github.com>
2021-10-27 13:59:27 -0400

    merger Ao

--- 01_SportsBetting.Data/04_Result.cs
+++ 01_SportsBetting.Data/04_Result.cs
@@ -10,16 +10,21 @@ namespace _01_SportsBetting.Data
 {
     public class Result
     {
+        // Key
         [Key]
         public int ResultId { get; set; }
 
+        // Foreign Keys
         [ForeignKey(nameof(UserId))]
         public int UserId { get; set; }
         [ForeignKey(nameof(BetId))]
         public int BetId { get; set; }
         [ForeignKey(nameof(TransactionId))]
+
+        // Virtual Lists
         public int TransactionId { get; set; }
 
+        // Variables
         public bool DidWin { get; set; }
         public DateTimeOffset CreatedUtc { get; set; }
         public DateTimeOffset ModifiedUtc { get; set; }
