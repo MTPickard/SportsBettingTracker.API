ddf77a052032b588e3d2ecb9a957c567947e3d75
XykWare <ethan.taylor.richardson@gmail.com>
2021-10-27 13:30:45 -0400

    updated book stuff

--- 02_SportsBetting.Models/02_BookEdit.cs
+++ 02_SportsBetting.Models/02_BookEdit.cs
@@ -1,36 +1,25 @@
-﻿using System;
+﻿using _01_SportsBetting.Data;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _01_SportsBetting.Data
+namespace _02_SportsBetting.Models
 {
-    public class Book
+    public class BookEdit
     {
-        [Key]
         public int BookId { get; set; }
-
-        [ForeignKey(nameof(UserId))]
-        public int UserId { get; set; }
 
         public virtual List<Transaction> _transactions { get; set; }
 
         public virtual List<Bet> _bets { get; set; }
 
-        [Required]
         public string Name { get; set; }
 
-        [Required]
         public double Balance { get; set; }
 
-        [Required]
         public string BookReference { get; set; }
-
-        [Required]
-        public DateTimeOffset CreatedUtc { get; set; }
 
         public DateTimeOffset ModifiedUtc { get; set; }
     }
